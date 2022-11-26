a07cf8b59f60e7ef50c05191f43c0def3d65f232
James Liu <james@jamesliu.io>
2022-11-26 15:32:57 +1100

    Add tick command and proxy config

--- main.go
+++ main.go
@@ -3,11 +3,33 @@ package main
 import (
 	"log"
 	"os"
+	"time"
 )
 
 func main() {
 	if len(os.Args[1:]) == 1 && os.Args[1] == "server" {
 		serve()
+	}
+
+	if len(os.Args[1:]) == 1 && os.Args[1] == "tick" {
+		log.Println("Periodically updating forecast every hour...")
+		ticker := time.NewTicker(1 * time.Hour)
+		quit := make(chan struct{})
+		go func() {
+			for {
+				select {
+				case <-ticker.C:
+					if err := forecast(); err != nil {
+						log.Printf("fetch forecast: %v", err)
+					} else {
+						log.Println("Successfully fetched forecast.")
+					}
+				}
+			}
+		}()
+
+		<-quit
+		return
 	}
 
 	if err := forecast(); err != nil {
